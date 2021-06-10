a289b2d8d311048d6d2bcf5983f95ae2b8d1ce76
mehrana1380 <84845533+mehrana1380@users.noreply.github.com>
2021-06-10 04:30:15 -0700

    Create halghe.py

---
+++ halghe.py
@@ -0,0 +1,11 @@
+start= 0
+while start <=100 :
+    print (start)
+    dastoor=input("aya bre bala?")
+    if dastoor== "are" :
+        start+= 1
+    if dastoor== "na" :
+        start-=1
+    if dastoor== "base":
+        break
+print("khodahafez")
